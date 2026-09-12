e8649fff6f0e61e5dc7bafb21006914b334c13e8
habibdefunc <devdothabib@gmail.com>
2026-09-12 14:41:31 +0700

    feat(api): setup cors for web

--- src/application/web.ts
+++ src/application/web.ts
@@ -2,8 +2,19 @@ import { publicRouter } from './../routes/publicApi';
 import { apiRouter } from './../routes/api';
 import express from 'express';
 import { errorMiddleware } from '../middleware/errorMiddleware';
+import cors from "cors"
 
 export const web = express()
+
+web.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://lpm-monitor-dashboard-web.vercel.app"
+    ],
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "X-API-TOKEN"],
+    exposedHeaders: ["Content-Disposition"]
+}))
 
 web.use(express.json())
 web.use(publicRouter)
